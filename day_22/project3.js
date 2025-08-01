744c31e751f9021a2db103ff037aeeeb2add0768
asad <asad28d@gmail.com>
2025-08-01 18:58:33 +0500

    completd level

--- day_22/project3.js
+++ day_22/project3.js
@@ -1,17 +1,17 @@
-let htmlContent = document.querySelectorAll('.wrapper')
+let htmlContent = document.querySelector('.wrapper')
+
 let challengeDivMain = document.querySelectorAll('#challengeDivMain')
+htmlContent.style.marginLeft = '300px'
+htmlContent.style.marginRight = '300px'
+htmlContent.style.fontFamily = 'sans-serif'
+
 let challengeDiv = document.querySelectorAll('.challengeDiv')
 let challengeP = document.querySelectorAll('.challengeP')
-console.log(challengeP)
 let headings = document.querySelectorAll('h1 , h2')
 let clockDiv = document.querySelectorAll('h4');
 
 
-htmlContent.forEach(item => {
-    item.style.marginLeft = '300px'
-    item.style.marginRight = '300px'
-    item.style.fontFamily = 'sans-serif'
-});
+
 
 headings.forEach(item => {
     item.style.textAlign = 'center'
@@ -52,21 +52,13 @@ challengeDivMain.forEach(item => {
     item.style.paddingInlineStart = '0px'
 })
 
-console.log()
-challengeDiv.forEach(div => {
-    div.style.display = 'flex'
-    div.style.flexWrap = 'wrap'
-    div.style.marginBottom = '3px'
-    let detailTag = document.createElement("details");
-    div.appendChild(detailTag)
-})
-
 let textContentString;
 let lastWord;
 
 challengeP.forEach((Item, index) => {
-    Item.style.padding = '16px'
+    Item.style.padding = '25px'
     Item.style.marginTop = '0px'
+    Item.style.marginBottom = '0px'
     Item.style.listStyleType = 'none'
     textContentString = Item.textContent.split(" ")
     lastWord = textContentString.pop()
@@ -132,10 +124,10 @@ const asabenehChallenges2020 = {
             ['📔', 'Content Creator']
         ],
         qualifications: [
-            'MSc. Computer Science Ongoing',
-            'BSc. Information and Communication Eng.',
-            'MSc. Food Technology',
-            'BSc.Food Technology'
+            ['📖','MSc. Computer Science Ongoing'],
+            ['👨‍🎓','BSc. Information and Communication Eng.'],
+            ['👨‍🎓','MSc. Food Technology'],
+            ['👨‍🎓','BSc.Food Technology']
         ],
         socialLinks: [
             {
@@ -153,19 +145,19 @@ const asabenehChallenges2020 = {
                 fontawesomeIcon: '<i class="fab fa-github-square"></i>',
                 url: 'https://github.com/Asabeneh'
             },
-            {
-                social: 'DEV.to',
-                fontawesomeIcon: '',
-                url: 'https://dev.to/asabeneh'
-            }
+            // {
+            //     social: 'DEV.to',
+            //     fontawesomeIcon: '',
+            //     url: 'https://dev.to/asabeneh'
+            // }
         ],
         skills: [
-            'Web Development',
-            'Data Analysis',
-            'Data Visualization',
-            'Programming',
-            'Databases',
-            'Developing API'
+            ['✅','Web Development'],
+            ['✅','Data Analysis'],
+            ['✅','Data Visualization'],
+            ['✅','Programming'],
+            ['✅','Databases'],
+            ['✅','Developing API'],
         ],
         bio:
             'I am an educator, developer, motivator and content creator. I am a life-long learner. If you like to know more about me checkout my LinkedIn or Github profile. Thank you so much for joining in my quest of changing everyone to developer.'
@@ -278,19 +270,147 @@ const asabenehChallenges2020 = {
     ]
 }
 
-for (let challenge of asabenehChallenges2020.challenges) {
-    let summarytag = document.createElement("summary");
-    let summarytext = document.createElement("p");
-    challengeTitle = challenge.name.split(" ").pop()
-    summarytag.textContent = challengeTitle
-    console.log(summarytag)
-    let skillTopics = challenge.topics.join("<br> ")
-    summarytext.textContent = skillTopics
+challengeDiv.forEach((div, index) => {
+    div.style.display = 'flex'
+    div.style.flexWrap = 'wrap'
+    div.style.marginBottom = '3px'
+    let challenge = asabenehChallenges2020.challenges[index]
+    let detailTag = document.createElement("details")
+    let summaryTag = document.createElement("summary")
+    let summaryText = document.createElement("p")
+
+    let challengeTitle = challenge.name.split(" ").pop()
+    summaryTag.innerHTML = challengeTitle
+
+    let skillTopics = challenge.topics.join("<br>")
+    summaryText.innerHTML = skillTopics
+
+    detailTag.style.padding = '25px'
+
+    detailTag.appendChild(summaryTag)
+    detailTag.appendChild(summaryText)
+
+    div.appendChild(detailTag)
+})
+
+let maindiv = document.createElement("div")
+maindiv.style.textAlign = 'center'
+htmlContent.appendChild(maindiv);
+
+let nameDiv = document.createElement("h1");
+function nameing(property) {
+    nameDiv.innerHTML = `${property.firstName} ` + ` ${property.lastName}`;
 }
+nameing(asabenehChallenges2020.author)
+maindiv.appendChild(nameDiv)
 
-// detailTag.forEach(item => {
-//     item.style.padding = '16px'
-//     item.appendChild(summarytag)
-//     item.appendChild(summarytext)
-// })
+let biotext = document.createElement('p')
+biotext.innerHTML = asabenehChallenges2020.author.bio;
+maindiv.appendChild(biotext)
 
+let authorInfoMain = document.createElement("div")
+authorInfoMain.style.display = 'flex'
+authorInfoMain.style.flexWrap = 'wrap'
+authorInfoMain.style.textAlign = 'start'
+let authorinfo1Div = document.createElement("div")
+authorinfo1Div.style.marginRight = "55px"
+let authorinfo1h = document.createElement("h3")
+authorinfo1h.innerHTML = "titles";
+let authorinfo1p = document.createElement("p")
+let titleContent = asabenehChallenges2020.author.titles.join("<br>")
+authorinfo1p.innerHTML = titleContent;
+authorinfo1Div.appendChild(authorinfo1h)
+authorinfo1Div.appendChild(authorinfo1p)
+authorInfoMain.appendChild(authorinfo1Div)
+maindiv.appendChild(authorInfoMain)
+
+let authorinfo2Div = document.createElement("div")
+authorinfo2Div.style.marginRight = "55px"
+let authorinfo2h = document.createElement("h3")
+authorinfo2h.innerHTML = "skills";
+let authorinfo2p = document.createElement("p")
+let skillsContent = asabenehChallenges2020.author.skills.join("<br>")
+authorinfo2p.innerHTML = skillsContent;
+authorinfo2Div.appendChild(authorinfo2h)
+authorinfo2Div.appendChild(authorinfo2p)
+authorInfoMain.appendChild(authorinfo2Div)
+maindiv.appendChild(authorInfoMain)
+
+let authorinfo3Div = document.createElement("div")
+authorinfo3Div.style.marginRight = '0px'
+let authorinfo3h = document.createElement("h3")
+authorinfo3h.innerHTML = "qualification";
+let authorinfo3p = document.createElement("p")
+let qualificationContent = asabenehChallenges2020.author.qualifications.join("<br>")
+authorinfo3p.innerHTML = qualificationContent;
+authorinfo3Div.appendChild(authorinfo3h)
+authorinfo3Div.appendChild(authorinfo3p)
+authorInfoMain.appendChild(authorinfo3Div)
+maindiv.appendChild(authorInfoMain)
+
+let keywordsH = document.createElement("h2")
+keywordsH.innerHTML = "keywords";
+keywordsH.style.textAlign = "start"
+maindiv.appendChild(keywordsH)
+
+let keywordsMain = document.createElement("div")
+keywordsMain.style.display = 'flex'
+keywordsMain.style.flexWrap = "wrap"
+
+let keywordBox = document.createElement("div")
+keywordBox.style.marginRight = '20px'
+keywordBox.style.paddingTop = '6px'
+keywordBox.style.paddingBottom = '6px'
+keywordBox.style.paddingRight = '20px'
+keywordBox.style.paddingLeft = '20px'
+
+keywordBox.style.borderRadius = '35%'
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+keywordBox.style.backgroundColor = getRandomColor()
+
+for (let i = 0; i < asabenehChallenges2020.keywords.length; i++) {
+    let keywordBox = document.createElement("div")
+    keywordBox.style.marginRight = '12px'
+    keywordBox.style.marginBottom = '10px'
+    keywordBox.style.paddingTop = '6px'
+    keywordBox.style.paddingBottom = '6px'
+    keywordBox.style.paddingRight = '20px'
+    keywordBox.style.paddingLeft = '20px'
+
+    keywordBox.style.borderRadius = '100px'
+    function getRandomColor() {
+        const letters = '0123456789ABCDEF';
+        let color = '#';
+        for (let i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
+    keywordBox.style.backgroundColor = getRandomColor()
+
+    keywordBox.innerHTML = "#" + asabenehChallenges2020.keywords[i]
+    keywordsMain.appendChild(keywordBox)
+
+}
+maindiv.appendChild(keywordsMain)
+
+
+
+
+// let iconsdiv = document.createElement("div")
+// iconsdiv.style.display = 'flex';
+// iconsdiv.style.flexWrap = 'wrap';
+// function addingicons(property) {
+//     for (let icon in property.socialLinks) {
+//         iconsdiv.innerHTML = icon;
+//     }
+// }
+// addingicons(asabenehChallenges2020.author)
+// maindiv.appendChild(iconsdiv)
