3ba6eca35329122f34662810eb5afa945ea943ec
asad <asad28d@gmail.com>
2025-08-07 18:06:32 +0500

    day28 updated

---
+++ day_28/day28.js
@@ -0,0 +1,5 @@
+let firstName = document.getElementById('first-name-input')
+let lastName = document.getElementById('last-name-input')
+let country = document.getElementById('country-input')
+let score = document.getElementById('score-input')
+
